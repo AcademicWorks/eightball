8c72d3423e94066ad12c489328dae4e2d91f11e1
Christopher J. Bottaro <cjbottaro@alumni.cs.utexas.edu>
2013-07-17 14:59:55 -0500

    Make work

--- lib/eightball.rb
+++ lib/eightball.rb
@@ -1,21 +1,48 @@
 require 'iconv'
 module Eightball
 
-	def eightball
-		string = self.encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace)
-		begin
-    		string.empty?
-  		rescue ArgumentError => e
-	    	if e.message == "invalid byte sequence in UTF-8"
-	      		Thread.current["iconv"] ||= Iconv.new('UTF-8//IGNORE', 'UTF-8')
-	      		string = Thread.current["iconv"].iconv(string)
-	    	else
-	      		raise
-	    	end
-  		end
-  		#drop the BOM
-    string.force_encoding("UTF-8").gsub("\xEF\xBB\xBF".force_encoding("UTF-8"), '')
-	end
+  def eightball
+    string = self
+    string = Helpers.version2(string) if Helpers.needs_help?(string)
+    string = Helpers.version1(string) if Helpers.needs_help?(string)
+    string
+  end
+
+  module Helpers
+    extend(self)
+
+    def needs_help?(string)
+      string.force_encoding(Encoding::UTF_8)
+      begin
+        string.sub("", "")
+      rescue ArgumentError => e
+        e.message == "invalid byte sequence in UTF-8"
+      else
+        false
+      end
+    end
+
+    def version1(string)
+      string = string.encode(Encoding::UTF_8, :invalid => :replace, :undef => :replace)
+      begin
+          string.empty?
+      rescue ArgumentError => e
+        if e.message == "invalid byte sequence in UTF-8"
+          Thread.current["iconv"] ||= Iconv.new('UTF-8//IGNORE', 'UTF-8')
+          string = Thread.current["iconv"].iconv(string)
+        else
+          raise
+        end
+      end
+      #drop the BOM
+      string.force_encoding("UTF-8").gsub("\xEF\xBB\xBF".force_encoding("UTF-8"), '')
+    end
+
+    def version2(string)
+      string.encode(Encoding::UTF_8, Encoding::ASCII_8BIT, :invalid => :replace, :undef => :replace)
+    end
+
+  end
 
 end
 
--- test/test_eightball.rb
+++ test/test_eightball.rb
@@ -3,8 +3,17 @@
 require 'helper'
 
 class TestEightball < Test::Unit::TestCase
+
   should "return utf-8 when utf-8 is called" do
     assert_equal "ウabcé", "ウabcé".encode("utf-8")
+  end
+
+  should "remove invalid UTF-8 byte sequences" do
+    string = File.read("test/files/test1")
+    assert Eightball::Helpers.needs_help?(string)
+    string = string.eightball
+    assert !string.empty?
+    assert !Eightball::Helpers.needs_help?(string)
   end
   
 end
